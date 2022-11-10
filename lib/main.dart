5afcb256a95c0d3b50297ee7061c0e9a21aa9fd3
ZARIFMAC <uzbzarif@gmail.com>
2022-11-10 14:56:02 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NavigationBar(),
+  ));
+}
+
+class NavigationBar extends StatelessWidget {
+  const NavigationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
